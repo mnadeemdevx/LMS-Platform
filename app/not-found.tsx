fc9ff7c1bd2ad7b17b86540fd4f0a7cd3fea6caa
Muhammad Nadeem <muhammadnadeem.1596@gmail.com>
2024-06-23 23:00:42 +0500

    update not-found file

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,17 +1,22 @@
-import { ArrowLeftCircleIcon } from "lucide-react";
 import Link from "next/link";
+
+import { ArrowLeftCircleIcon } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
 
 const NotFound = () => {
     return (
         <main className="not-found">
-            <h1 className="text-sky-400">Not Found</h1>
+            <h1 className="text-[#0369a1]">Not Found</h1>
             <p className="text-slate-700">
                 Unfortunately, we could not find the requested page or resource.
             </p>
-            <Link href={"/"} className="flex items-center gap-x-2">
-                <ArrowLeftCircleIcon size={40} className="text-sky-400" />
-                Get back
-            </Link>
+            <Button variant="outline">
+                <Link href={"/"} className="flex items-center gap-x-2">
+                    <ArrowLeftCircleIcon size={20} className="text-[#0369a1]" />
+                    Get back
+                </Link>
+            </Button>
         </main>
     );
 };
